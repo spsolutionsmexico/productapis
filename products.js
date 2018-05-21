affd03a2235427bbf2e009887da54a3b30d5fc5f
israel125x <30633437+israel125x@users.noreply.github.com>
2018-05-21 18:17:40 -0500

    test 4
    test 4

--- products.js
+++ products.js
@@ -1,15 +1,15 @@
-var cloudant = require('cloudant')(cloudantService.credentials.url);
+/*var cloudant = require('cloudant')(cloudantService.credentials.url);
 exports.cloudant = cloudant;
 var productDb = cloudant.use('products');
 exports.productDb = productDb;
-
+*/
 //list all the database contents.
-exports.list = function(req, res) {
-    productDb.list({include_docs: true}, function(err, body/*, headers*/) {
+/*exports.list = function(req, res) {
+    productDb.list({include_docs: true}, function(err, body/*, headers+/) {
 	    if (!err) {
 	        res.send(body);
 	        return;
 	    }
 	   	res.send({msg:'Error listing items: ' + err});
     });
-};
+};*/
--- server.js
+++ server.js
@@ -3,16 +3,16 @@ var app = express();
 var cfenv = require("cfenv");
 
 var appEnv = cfenv.getAppEnv();
-cloudantService = appEnv.getService("TestDB");
+//cloudantService = appEnv.getService("TestDB");
 
 /******************************
  * APIs 
  *******************************/
 
-var products = require("./products");
+/*var products = require("./products");
 app.get('/api/v1/products', function(req, res) {
     products.list(req, res);
-});
+});*/
 app.get('/api/v1/welcome', function(req, res) {
     res.send("Hello Sunil");
 });
