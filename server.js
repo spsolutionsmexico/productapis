368fefa42536ef0349045e6ed2c9570004671d83
israel125x <30633437+israel125x@users.noreply.github.com>
2018-05-21 18:03:59 -0500

    test 1
    test 1

--- server.js
+++ server.js
@@ -3,22 +3,21 @@ var app = express();
 var cfenv = require("cfenv");
 
 var appEnv = cfenv.getAppEnv();
-cloudantService = appEnv.getService("productDB");
+cloudantService = appEnv.getService("TestDB");
 
 
 /******************************
-* APIs 
-*******************************/
+ * APIs 
+ *******************************/
 
 var products = require("./products");
 app.get('/api/v1/products', function(req, res) {
-  products.list(req,res);
+    products.list(req, res);
 });
 app.get('/api/v1/welcome', function(req, res) {
-	res.send("Hello Sunil");
+    res.send("Hello Sunil");
 });
 var port = process.env.PORT || 9080;
 app.listen(port, function() {
     console.log('App started on port:' + port);
 });
-
