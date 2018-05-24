a237ac7db362d72ecc2c435cf6e1b1b5b0335ed9
israel125x <30633437+israel125x@users.noreply.github.com>
2018-05-24 13:05:04 -0500

    test 12
    -12-

--- server.js
+++ server.js
@@ -8,32 +8,12 @@ var appEnv = cfenv.getAppEnv();
 app.get('/api/v1/welcome', function(req, res) {
     res.send("Hello User");
 });
-app.get('/api/ift/:telefono', (req, res) {
 
+app.get('/api/ift/:telefono', function(req, res) {
     var telefono = req.params.telefono;
-    var web = "http://office.ekiwi.net:8081/MonteDePiedadAPI/runSnsIft?apiKey=UhTtDEtCgAwTQ2y6htVKV9BL&host=localhost&port=8799&phone=" + telefono;
-
-    var promise = new Promise(function(resolve, reject) {
-        request({
-            method: "GET",
-            url: web,
-            headers: {
-                "Content-Type": "application/json"
-            }
-        }, function(error, res, body) {
-            console.log(body);
-            if (error) reject(error);
-            else resolve(body);
-        });
-    });
-    promise.then(function(response) {
-        var resp = JSON.parse(response);
-        console.log(resp);
-    }).then(function(respuesta) {
-        res.send(respuesta);
-    });
-
+    res.send("Telefono = " + telefono);
 });
+
 var port = process.env.PORT || 9080;
 app.listen(port, function() {
     console.log('App started on port:' + port);
